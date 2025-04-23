6ed67dee49dc65db1e87241f50fc5282b7541cb3
Jack Goodman <jgoodman@rittal-csm.co.uk>
2025-04-23 13:40:49 +0100

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Jack Goodman | Personal Porfolio',
+  description: 'Jack is hungry',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={'${inter.className} bg-gray-50'}>
+        {children}
+        
+        </body>
+    </html>
+  )
+}
